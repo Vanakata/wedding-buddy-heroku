fd72ca021356bd99362369994e41c2092ec6e96e
Vanakata <39580439+Vanakata@users.noreply.github.com>
2020-01-06 19:34:43 +0200

    Update database.js

--- src/configuration/database.js
+++ src/configuration/database.js
@@ -4,7 +4,7 @@ const User = require('../models/user');
 mongoose.Promise = global.Promise;
 
 module.exports = (configurationObject) => {
-    mongoose.connect(configurationObject.dbPath,{ useNewUrlParser: true,useCreateIndex: true, });
+    mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/MyWeddingSite',{ useNewUrlParser: true,useCreateIndex: true, });
     let db = mongoose.connection;
 
     db.once('open', err => {
